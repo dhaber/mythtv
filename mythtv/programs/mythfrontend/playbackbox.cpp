c8abc4a0b7a47b8159267bec3c8bc4f15f94f35e
Doug Haber <doug@fawnanddoug.com>
2011-07-25 18:34:05 -0400

    mythweb + bugs

--- mythtv/programs/mythfrontend/playbackbox.cpp
+++ mythtv/programs/mythfrontend/playbackbox.cpp
@@ -1286,9 +1286,16 @@ void PlaybackBox::updateRecList(MythUIButtonListItem *sel_item)
         item->SetTextFromMap(infoMap);
 
         QString tempSubTitle  = extract_subtitle(**it, groupname);
+        QString tempShortDate = MythDateTimeToString((*it)->GetRecordingStartTime(),
+                                                     kDateShort);
+        QString tempLongDate  = MythDateTimeToString((*it)->GetRecordingStartTime(),
+                                                     kDateFull | kSimplify);
 
         if (groupname == (*it)->GetTitle().toLower())
             item->SetText(tempSubTitle,       "titlesubtitle");
+
+        item->SetText(tempLongDate,       "longdate");
+        item->SetText(tempShortDate,      "shortdate");
 
         item->DisplayState(state, "status");
 
