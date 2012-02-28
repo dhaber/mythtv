f85146fd4eeceee586aff3ca1a9f6ec91390d57c
Doug Haber <doug@fawnanddoug.com>
2012-02-28 13:15:50 -0500

    undo

--- mythtv/programs/mythmetadatalookup/lookup.cpp
+++ mythtv/programs/mythmetadatalookup/lookup.cpp
@@ -343,8 +343,9 @@ void LookerUpper::customEvent(QEvent *levent)
             if (rule)
             {
                 rule->LoadByProgram(pginfo);
-                if (rule->m_inetref.isEmpty() &&
-                    (rule->m_searchType == kNoSearch))
+                if (rule->m_inetref.isEmpty())
+//                if (rule->m_inetref.isEmpty() &&
+//                    (rule->m_searchType == kNoSearch))
                 {
                     rule->m_inetref = lookup->GetInetref();
                 }
